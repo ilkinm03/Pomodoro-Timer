d3afb17dc6c52a23818dc1a40932f27e342eff2e
ilkinm03 <ilkinm2003@gmail.com>
2023-04-02 14:18:21 +0400

    create tomato canvas with countdown text

--- main.py
+++ main.py
@@ -9,4 +9,11 @@ head_label = Label(text="Timer", bg=YELLOW, fg=GREEN)
 head_label.config(font=(FONT_NAME, 40, "bold"))
 head_label.grid(column=1, row=0)
 
+canvas = Canvas(width=200, height=224)
+canvas.config(bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_count = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
 window.mainloop()
