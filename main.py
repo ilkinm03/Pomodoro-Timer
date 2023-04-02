c78b74c77232e39aef40eb45c0f676676b64da5d
ilkinm03 <ilkinm2003@gmail.com>
2023-04-02 14:17:53 +0400

    create head label with "Timer" text

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+head_label = Label(text="Timer", bg=YELLOW, fg=GREEN)
+head_label.config(font=(FONT_NAME, 40, "bold"))
+head_label.grid(column=1, row=0)
+
 window.mainloop()
